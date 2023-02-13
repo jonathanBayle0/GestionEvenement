15c5fc86660e55d92f4732df63a53cdd37e03689
jonathanBayle0 <jonathan.bayle002@gmail.com>
2023-02-13 14:55:12 +0100

    Initialisation du projet et des POJO

--- src/main/java/donnees/Information.java
+++ src/main/java/donnees/Information.java
@@ -1,119 +1,106 @@
 package donnees;
 
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import java.util.List;
+import java.util.Objects;
 
-@Entity
-@Table(name = "t_information_inf", schema = "gestionevenement", catalog = "")
+import org.bson.types.ObjectId;
 public class Information {
-    @Basic
-    @Column(name = "_id")
-    private String id;
-    @Basic
-    @Column(name = "inf_cible")
-    private Integer infCible;
-    @Basic
-    @Column(name = "inf_contenu")
-    private String infContenu;
-    @Basic
-    @Column(name = "inf_date")
-    private String infDate;
-    @Basic
-    @Column(name = "inf_nom")
-    private String infNom;
-    @Basic
-    @Column(name = "inf_type")
-    private String infType;
-    @Basic
-    @Column(name = "inf_type_contenu")
-    private String infTypeContenu;
+    private ObjectId id;
 
-    public String getId() {
+    private Short inf_cible;
+
+    private String inf_contenu;
+
+    private String inf_date;
+
+    private String inf_nom;
+
+    private String inf_type;
+
+    private String inf_type_contenu;
+
+
+    public ObjectId getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(ObjectId id) {
         this.id = id;
     }
 
-    public Integer getInfCible() {
-        return infCible;
+    public Short getInf_cible() {
+        return inf_cible;
     }
 
-    public void setInfCible(Integer infCible) {
-        this.infCible = infCible;
+    public void setInf_cible(Short inf_cible) {
+        this.inf_cible = inf_cible;
     }
 
-    public String getInfContenu() {
-        return infContenu;
+    public String getInf_contenu() {
+        return inf_contenu;
     }
 
-    public void setInfContenu(String infContenu) {
-        this.infContenu = infContenu;
+    public void setInf_contenu(String inf_contenu) {
+        this.inf_contenu = inf_contenu;
     }
 
-    public String getInfDate() {
-        return infDate;
+    public String getInf_date() {
+        return inf_date;
     }
 
-    public void setInfDate(String infDate) {
-        this.infDate = infDate;
+    public void setInf_date(String inf_date) {
+        this.inf_date = inf_date;
     }
 
-    public String getInfNom() {
-        return infNom;
+    public String getInf_nom() {
+        return inf_nom;
     }
 
-    public void setInfNom(String infNom) {
-        this.infNom = infNom;
+    public void setInf_nom(String inf_nom) {
+        this.inf_nom = inf_nom;
     }
 
-    public String getInfType() {
-        return infType;
+    public String getInf_type() {
+        return inf_type;
     }
 
-    public void setInfType(String infType) {
-        this.infType = infType;
+    public void setInf_type(String inf_type) {
+        this.inf_type = inf_type;
     }
 
-    public String getInfTypeContenu() {
-        return infTypeContenu;
+    public String getInf_type_contenu() {
+        return inf_type_contenu;
     }
 
-    public void setInfTypeContenu(String infTypeContenu) {
-        this.infTypeContenu = infTypeContenu;
+    public void setInf_type_contenu(String inf_type_contenu) {
+        this.inf_type_contenu = inf_type_contenu;
+    }
+
+    public Information(ObjectId id, Short inf_cible, String inf_contenu, String inf_date, String inf_nom, String inf_type, String inf_type_contenu) {
+        super();
+        this.id = id;
+        this.inf_cible = inf_cible;
+        this.inf_contenu = inf_contenu;
+        this.inf_date = inf_date;
+        this.inf_nom = inf_nom;
+        this.inf_type = inf_type;
+        this.inf_type_contenu = inf_type_contenu;
+    }
+
+    public Information() {
+        super();
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
+        if (!(o instanceof Information)) return false;
         Information that = (Information) o;
-
-        if (id != null ? !id.equals(that.id) : that.id != null) return false;
-        if (infCible != null ? !infCible.equals(that.infCible) : that.infCible != null) return false;
-        if (infContenu != null ? !infContenu.equals(that.infContenu) : that.infContenu != null) return false;
-        if (infDate != null ? !infDate.equals(that.infDate) : that.infDate != null) return false;
-        if (infNom != null ? !infNom.equals(that.infNom) : that.infNom != null) return false;
-        if (infType != null ? !infType.equals(that.infType) : that.infType != null) return false;
-        if (infTypeContenu != null ? !infTypeContenu.equals(that.infTypeContenu) : that.infTypeContenu != null)
-            return false;
-
-        return true;
+        return getId().equals(that.getId()) && Objects.equals(getInf_cible(), that.getInf_cible()) && Objects.equals(getInf_contenu(), that.getInf_contenu()) && Objects.equals(getInf_date(), that.getInf_date()) && Objects.equals(getInf_nom(), that.getInf_nom()) && Objects.equals(getInf_type(), that.getInf_type()) && Objects.equals(getInf_type_contenu(), that.getInf_type_contenu());
     }
 
     @Override
     public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (infCible != null ? infCible.hashCode() : 0);
-        result = 31 * result + (infContenu != null ? infContenu.hashCode() : 0);
-        result = 31 * result + (infDate != null ? infDate.hashCode() : 0);
-        result = 31 * result + (infNom != null ? infNom.hashCode() : 0);
-        result = 31 * result + (infType != null ? infType.hashCode() : 0);
-        result = 31 * result + (infTypeContenu != null ? infTypeContenu.hashCode() : 0);
-        return result;
+        return Objects.hash(getId(), getInf_cible(), getInf_contenu(), getInf_date(), getInf_nom(), getInf_type(), getInf_type_contenu());
     }
 }
